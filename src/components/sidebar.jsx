d1ac36c792ab4d0b419f9523dc014530b2356fd9
saravanaecom <saravanaecomweb@gmail.com>
2025-04-11 11:21:58 +0530

    Add files via upload

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -74,7 +74,7 @@ const [isProductsOpen, setIsProductsOpen] = useState(false);
     <>
     <div className='flex ' >
   <div
-        className={`bg-[var(--primary-bg-color)] text-gray-100 w-64 min-h-screen transform ${
+        className={`bg-[#0166ff] text-gray-100 w-64 min-h-screen transform ${
           isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
         } md:translate-x-0 transition-transform duration-300`}
       >
