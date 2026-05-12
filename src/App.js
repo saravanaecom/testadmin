20e588edd7b17e010b84091a423eaf24c405b193
saravanaecom <saravanaecomweb@gmail.com>
2026-05-12 13:49:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import Router from '../src/routes/Routes'
+import { AuthProvider } from './context/authContext'; 
 import './App.css';
-import './index.css';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import AppRouter from './routes/Routes';
+
 
 function App() {
   return (
     <>
-      <AppRouter/>
-    </>
+     <AuthProvider>
+     <Router/>
+    </AuthProvider>
+    
+  </>
   );
 }
 
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,33 +1,50 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
+// Create a context for authentication
 const AuthContext = createContext(null);
 
+// Custom hook to access the AuthContext
 export const useAuth = () => useContext(AuthContext);
 
+// AuthProvider component that provides authentication state
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isAuthenticatedName, setIsAuthenticatedName] = useState(false);
+  const [isAuthenticatedName, setIsAuthenticatedName] = useState('');
 
+  // Use useEffect to check authentication status on component mount
   useEffect(() => {
-    const userLogin = localStorage.getItem('userLogin') === 'true';  
-    const userName = localStorage.getItem('userName');  
+    const userLogin = localStorage.getItem('userLogin') === 'true';  // Check if user is logged in
+    const userName = localStorage.getItem('userName');  // Get the encoded username
     setIsAuthenticated(userLogin);
-    setIsAuthenticatedName(atob(userName));
+    if (userName) {
+      setIsAuthenticatedName((userName));  // Decode username from base64 if exists
+    }
   }, []);
-  
 
-  const userLogin = () => {
+  // Function to log in the user
+  const userLogin = (username) => {
     setIsAuthenticated(true);
-    localStorage.setItem('userLogin', btoa('true'));
+    localStorage.setItem('userLogin', 'true'); 
+    localStorage.setItem('userName', (username)); 
   };
+
   
   const userLogout = () => {
     setIsAuthenticated(false);
     localStorage.removeItem('userLogin');
-  };  
+    localStorage.removeItem('userName');
+  };
 
+  // Provide the authentication values to the context
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, isAuthenticatedName, setIsAuthenticatedName, userLogin, userLogout }}>
+    <AuthContext.Provider value={{ 
+      isAuthenticated, 
+      setIsAuthenticated, 
+      isAuthenticatedName, 
+      setIsAuthenticatedName, 
+      userLogin, 
+      userLogout 
+    }}>
       {children}
     </AuthContext.Provider>
   );
